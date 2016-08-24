f1337cf2fd9ab6051ef3e23f96fe2251bc32e362
qiu-deqing <deqing.qdq@alibaba-inc.com>
2016-08-24 11:27:21 +0800

    修改协议为https

--- magix-inspector/js/content.js
+++ magix-inspector/js/content.js
@@ -1,5 +1,5 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
  chrome.tabs.executeScript({
-  code: "javascript:void((function(d,s){s=d.createElement('script');s.src='//thx.github.io/magix-inspector/index.js';s.charset='utf-8';d.body.appendChild(s)}(document)))"
+  code: "javascript:void((function(d,s){s=d.createElement('script');s.src='https://thx.github.io/magix-inspector/index.js';s.charset='utf-8';d.body.appendChild(s)}(document)))"
 });
 });
